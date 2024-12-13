3659f4532ec7359e1bcba4e6897cae2c0e981946
Kitsomboon <136412015+Kitsomboon@users.noreply.github.com>
2024-12-13 16:51:33 +0700

    add13

--- src/service/About7.jsx
+++ src/service/About7.jsx
@@ -2,9 +2,9 @@
 
 function About7() {
     return (
-      <div className=" flex  h-screen bg-gray-100">
-        <div className="flex items-center bg-green-500 rounded-lg p-4 shadow-md">
-          <div className="w-16 h-16 rounded-full overflow-hidden mr-4">
+      <div className=" flex justify-center items-center bg-gray-100 py-20">
+        <div className="container flex items-center bg-green-500 rounded-lg p-4 shadow-md w-[500px] h-[100px]">
+          <div className="w-16 h-16 rounded-full overflow-hidden mr-4 rounded-lg margin-right-[20px]">
             <img
               src="https://via.placeholder.com/64" // Replace this with your image source
               alt="Avatar"
